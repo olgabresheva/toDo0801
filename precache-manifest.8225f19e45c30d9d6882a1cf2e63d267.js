a69323388704eea36c1555ca920cf9ef11aabe6c
olgab <olgabresheva7@gmail.com>
2020-08-02 23:32:27 -0700

    Updates

---
+++ precache-manifest.8225f19e45c30d9d6882a1cf2e63d267.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09d5af14b27a64e405840a88dda865ed",
+    "url": "/toDo0801/index.html"
+  },
+  {
+    "revision": "d9df3dfe7c1d85b0debf",
+    "url": "/toDo0801/static/css/main.c992bf6e.chunk.css"
+  },
+  {
+    "revision": "771c4b3f9a9c8b3b4563",
+    "url": "/toDo0801/static/js/2.23848f5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/toDo0801/static/js/2.23848f5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9df3dfe7c1d85b0debf",
+    "url": "/toDo0801/static/js/main.d6323b31.chunk.js"
+  },
+  {
+    "revision": "c8ff4fd9ae32a26db677",
+    "url": "/toDo0801/static/js/runtime-main.80da8224.js"
+  }
+]);
